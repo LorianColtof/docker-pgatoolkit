1a5cf72e8e1713cf929b2e7cd9aa6d39f3c7f529
Lorian Coltof <loriancoltof@gmail.com>
2017-02-21 15:39:44 +0100

    Add build.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 xhost +
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/pga -e DISPLAY=unix$DISPLAY --rm -i -t moretea/pgatoolkit bash -c "cd /pga; bash"
 
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v $PWD:/pga -e DISPLAY=unix$DISPLAY --rm -i -t pgatoolkit bash -c "cd /pga; bash"
 xhost -
